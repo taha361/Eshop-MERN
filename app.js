2bd79aa567bccda26ecf887fe6fb36cf00f52699
taha361 <tahayassinebenameur36@gmail.com>
2021-07-26 01:53:40 +0100

    finished backend

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require('./helpers/error-handler');
+
 
 app.use(cors());
 app.options("*", cors());
@@ -11,6 +14,9 @@ app.options("*", cors());
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
